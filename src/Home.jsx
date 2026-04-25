94f34db2df372d3654427fee4a3324e40788034e
hadiafyouni <hadiafyouni9@gmail.com>
2026-04-25 22:18:34 +0300

    edited the photos in the top product section and some product's card css

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,10 +2,9 @@ import "./Home.css";
 import WelcomeSection from "./WelcomeSection";
 import GeneralInfo from "./GeneralInfo";
 import ProductCard from "./ProductCard";
-import img1 from "./assets/IMG_4549.png";
-import img2 from "./assets/IMG_4605.png";
-import img3 from "./assets/IMG_4606.png";
-import img4 from "./assets/IMG_4607.png";
+import img1 from "./assets/IMG_15.png";
+import img2 from "./assets/IMG_13.png";
+
 
 export default function Home({ isOpen }) {
     return (
@@ -19,26 +18,14 @@ export default function Home({ isOpen }) {
                     <ProductCard
                         image={img1}
                         name="Classic Matcha"
-                        price="$29.99"
+                        price="$10"
                         description="Premium ceremonial grade for an authentic matcha experience."
                     />
                     <ProductCard
                         image={img2}
                         name="Banana Bread"
-                        price="$19.99"
+                        price="$12"
                         description="Perfect for baking and making delicious matcha lattes."
-                    />
-                    <ProductCard
-                        image={img3}
-                        name="Strawberry Jam"
-                        price="$24.99"
-                        description="Authentic bamboo whisk and scoop for the perfect froth."
-                    />
-                    <ProductCard
-                        image={img4}
-                        name="Vanilla Matcha"
-                        price="$45.00"
-                        description="Everything you need to begin your matcha journey."
                     />
                 </div>
             </div>
